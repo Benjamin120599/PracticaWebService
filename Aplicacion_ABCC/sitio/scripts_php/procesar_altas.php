e795fb0174f1996df4e31cdb5293defab086abae
Benjamin Viramontes <benja120599@gmail.com>
2020-11-13 17:54:54 -0600

    Se añadió el archivo procesar_altas

---
+++ Aplicacion_ABCC/sitio/scripts_php/procesar_altas.php
@@ -0,0 +1,20 @@
+<?php
+
+    include('alumnoDAO.php');
+
+    //Validación de datos
+
+    $nc = $_POST['caja_num_control'];
+    $n = $_POST['caja_nombre'];
+    $pa = $_POST['caja_primer_ap'];
+    $sa = $_POST['caja_segundo_ap'];
+    $e = $_POST['caja_edad'];
+    $s = 5;//$_POST['caja_semestre'];
+    $c = $_POST['caja_carrera'];
+
+    $aDAO = new AlumnoDAO();
+
+    $aDAO->agregarAlumnos($nc, $n, $pa, $sa, $e, $s, $c);
+
+
+?>
