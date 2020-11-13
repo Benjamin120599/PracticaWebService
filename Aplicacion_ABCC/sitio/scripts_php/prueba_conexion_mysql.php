743e6942c39ada352eef77fd2c87d172b3bf9e97
Benjamin Viramontes <benja120599@gmail.com>
2020-11-13 17:54:34 -0600

    se añadió el archivo para probar la conexion

---
+++ Aplicacion_ABCC/sitio/scripts_php/prueba_conexion_mysql.php
@@ -0,0 +1,26 @@
+<?php
+    /*include('conexion_bd.php');
+    //include_once
+    //require
+    //require_once
+
+    $con = new ConexionBD();
+    
+    $conexion = $con->getConexion();
+
+    var_dump($conexion);
+
+    */
+
+    include('conexion_bd.php');
+    //include_once
+    //require
+    //require_once
+
+    $con= new ConexionBD();
+
+    $conexion = $con->getConexion();
+
+    var_dump($conexion);
+
+?>
